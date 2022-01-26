5e1801008d39f6b7a310acb22cef9cf8c59449b6
Stacey Brinkley <brinkleystacey12@gmail.com>
2022-01-26 17:02:03 -0500

    about page started

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,26 @@
+import { useState, useEffect } from "react";
 
-function About(props) {
-    return <h1>About</h1>;
-  }
-  
-  export default About;
-  
+
+function About({ URL }) {
+    const [about, setAbout] = useState(null);
+
+    const getAboutData = async () => {
+        const response = await fetch(URL + "about");
+        const data = await response.json();
+        setAbout(data)
+    }
+
+    useEffect(() => getAboutData(), [])
+
+    const loaded = () => (
+        <div>
+            <h2>{about.name}</h2>
+            <h3>{about.email}</h3>
+            <p>{about.bio}</p>
+        </div>
+    );
+
+    return about ? loaded() : <h1>Loading...</h1>;
+}
+
+export default About;
