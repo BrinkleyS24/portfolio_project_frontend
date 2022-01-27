4a55bc3e2c163fca89aa8019a76ff9d69f86c3b4
Stacey Brinkley <brinkleystacey12@gmail.com>
2022-01-27 09:38:41 -0500

    frontend complete

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,7 +1,34 @@
+import { useState, useEffect } from "react";
+import { Button } from 'react-bulma-components';
 
-function Projects(props) {
-    return <h1>Projects</h1>;
-  }
-  
-  export default Projects;
-  
+
+function Projects({ URL }) {
+    const [projects, setProjects] = useState(null)
+
+    const getProjectsData = async () => {
+        const response = await fetch(URL + "projects");
+        const data = await response.json();
+        setProjects(data)
+    }
+
+    useEffect(() => getProjectsData(), []);
+
+    const loaded = () => {
+        return projects.map((project) => (
+          <div>
+            <h1>{project.name}</h1>
+            <img src={project.image} />
+            <a href={project.git}>
+              <button>Github</button>
+            </a>
+            <a href={project.live}>
+              <button>Live site</button>
+            </a>
+          </div>
+        ));
+      };
+    
+      return projects ? loaded() : <h1>Loading...</h1>;
+    }
+
+export default Projects;
